3b5003b82c0137d87a2af890f78e104c3f995446
farelTI <123742844+farelTI@users.noreply.github.com>
2025-01-09 23:09:57 +0700

    model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,17 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile()
+	{
+		return $this->hasOne(MProfiles::class);
+	}
+    public function review()
+	{
+		return $this->hasMany(MReviews::class);
+	}
+    public function role()
+	{
+		return $this->belongsTo(MRoles::class);
+	}
 }
